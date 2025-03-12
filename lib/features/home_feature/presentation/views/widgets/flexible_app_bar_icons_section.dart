c8d6114cb0388a39665c799311c369dfc84387e8
Mahmoud-Magdy17 <MahoudMagdy176203@gmail.com>
2025-03-12 23:54:37 +0200

    dart fix

--- lib/features/home_feature/presentation/views/widgets/flexible_app_bar_icons_section.dart
+++ lib/features/home_feature/presentation/views/widgets/flexible_app_bar_icons_section.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smile_simulation/core/utils/app_colors.dart';
 import 'package:smile_simulation/features/home_feature/presentation/views/widgets/custom_icon.dart';
-import 'package:smile_simulation/features/search_feature/presentation/views/searchview.dart';
 
 class FlexibleAppBarIconsSection extends StatelessWidget {
   const FlexibleAppBarIconsSection({super.key});
