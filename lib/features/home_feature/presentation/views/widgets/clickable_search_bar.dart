df1437b1b124ddd19a84eab9d4367a9eb31d49de
Mahmoud-Magdy17 <MahoudMagdy176203@gmail.com>
2025-03-14 14:20:41 +0200

    solve clickable search bar issue

--- lib/features/home_feature/presentation/views/widgets/clickable_search_bar.dart
+++ lib/features/home_feature/presentation/views/widgets/clickable_search_bar.dart
@@ -8,49 +8,45 @@ import 'package:smile_simulation/features/home_feature/presentation/views/widget
 import 'package:smile_simulation/features/home_feature/presentation/views/widgets/custom_icon.dart';
 
 class ClickableSearchBar extends StatelessWidget {
-  const ClickableSearchBar({super.key});
+  ClickableSearchBar({super.key});
+  final Function(BuildContext) onTap = (context) {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => CreatePostView()));
+  };
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: ,
-      decoration: BoxDecoration(
-        color: AppColors.whiteColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-      child: Row(
-        children: [
-          CurrentUserCircleImage(
-            color: AppColors.primaryColor,
-            borderWidth: 6,
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) {
-              //       return UserAccountView();
-              //     },
-              //   ),
-              // );
-            },
-          ),
-          SizedBox(width: 4),
-          Expanded(
-            child: Container(
-              height: 36,
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              alignment: Alignment.centerRight,
-              decoration: BoxDecoration(
-                color: AppColors.veryLightGreyColor,
-                borderRadius: BorderRadius.circular(6),
-              ),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(
-                    context,
-                  ).push(MaterialPageRoute(builder: (context) => CreatePostView()));
-                },
+    return InkWell(
+      onTap: () {
+        onTap(context);
+      },
+      child: Container(
+        // height: ,
+        decoration: BoxDecoration(
+          color: AppColors.whiteColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+        child: Row(
+          children: [
+            CurrentUserCircleImage(
+              color: AppColors.primaryColor,
+              borderWidth: 6,
+              onTap: () {
+                onTap(context);
+              },
+            ),
+            SizedBox(width: 4),
+            Expanded(
+              child: Container(
+                height: 36,
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                alignment: Alignment.centerRight,
+                decoration: BoxDecoration(
+                  color: AppColors.veryLightGreyColor,
+                  borderRadius: BorderRadius.circular(6),
+                ),
                 child: Text(
                   LocalizationHelper.translate(TranslationKeys.createNewPost),
                   style: AppTextStyles.style14W400(
@@ -59,14 +55,16 @@ class ClickableSearchBar extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          SizedBox(width: 10),
-          CustomIcon(
-            icon: Icons.image,
-            color: AppColors.primaryColor,
-            onTap: () {},
-          ),
-        ],
+            SizedBox(width: 10),
+            CustomIcon(
+              icon: Icons.image,
+              color: AppColors.primaryColor,
+              onTap: () {
+                onTap(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
