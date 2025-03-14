2b28ef5c0cfe63d8945d68b65549bb98dd29572f
Mahmoud-Magdy17 <MahoudMagdy176203@gmail.com>
2025-03-14 14:58:15 +0200

    add localization flutter_intl and setup to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:smile_simulation/core/database/cache/cache_helper.dart';
 import 'package:smile_simulation/core/services/app_keys.dart';
 import 'package:smile_simulation/core/utils/app_colors.dart';
 import 'package:smile_simulation/features/home_feature/presentation/views/home_view.dart';
+import 'package:smile_simulation/generated/l10n.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +18,13 @@ class SmileSimulation extends StatelessWidget {
   Widget build(BuildContext context) {
     CacheHelper().saveData(key: AppKeys.languageCode, value: 'ar');
     return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       locale: Locale('ar'),
       title: 'Smile Simulation',
       color: AppColors.primaryColor,
