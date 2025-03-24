9c8ef1d9264d7ffccac800c7c6e6717ebe0c7d23
mohamed <mezoattalla123@gmail.com>
2025-03-24 16:53:36 +0200

    ✨ [FEAT](auth/login): Refactor login view to use smaller widgets

--- lib/features/auth/login/presentation/view/login_view.dart
+++ lib/features/auth/login/presentation/view/login_view.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:smile_simulation/core/utils/app_colors.dart';
-import 'package:smile_simulation/core/utils/app_text_styles.dart';
-import 'package:smile_simulation/core/widgets/custom_auth_appbar.dart';
-import 'package:smile_simulation/core/widgets/custom_body_screen.dart';
-import 'package:smile_simulation/core/widgets/custom_button.dart';
-import 'package:smile_simulation/core/widgets/custom_password_itext_field.dart';
-import 'package:smile_simulation/core/widgets/custom_text_field.dart';
+import 'package:smile_simulation/features/auth/login/presentation/view/widgets/login_view_body.dart';
 
-import 'package:smile_simulation/core/widgets/bottom_navigation_bar/bottom_nvaigation_view.dart';
-
-import 'package:smile_simulation/generated/assets.dart';
-
-import '../../../../../constant.dart';
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_text_styles.dart';
+import '../../../../../core/widgets/custom_auth_appbar.dart';
 import '../../../../../generated/l10n.dart';
-import '../../../sign_up/presentation/view/forgot_view.dart';
 import '../../../sign_up/presentation/view/manage_sign_up.dart';
-import 'widgets/media_button.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -52,96 +41,7 @@ class LoginView extends StatelessWidget {
           ),
         ),
       ),
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: CustomBodyScreen(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 32),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Flexible(
-                          child: Text(
-                            S.of(context).welcomeBack,
-                            style: AppTextStyles.headline1(
-                              context,
-                            ).copyWith(color: Color(0xFF4F4F4F)),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        SizedBox(width: 8),
-                        SvgPicture.asset(Assets.imagesEmojiSmiling),
-                      ],
-                    ),
-                  ],
-                ),
-
-                SizedBox(height: 56),
-                CustomTextField(
-                  title: S.of(context).email,
-                  hintText: "example@gmail.com",
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(height: 32),
-                CustomPasswordTextField(
-                  hintText: "*********",
-                  title: S.of(context).password,
-                ),
-                Align(
-                  alignment:
-                      isArabic == 'ar'
-                          ? Alignment.centerLeft
-                          : Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, ForgetView.routeName);
-                    },
-                    child: Text(
-                      S.of(context).forgotPassword,
-                      style: AppTextStyles.caption1(context),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 56),
-                CustomButton(
-                  title: S.of(context).signIn,
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(
-                      context,
-                      BottomNavigationView.routeName,
-                    );
-                  },
-                ),
-                SizedBox(height: 44),
-
-                Text(
-                  S.of(context).signInWith,
-                  style: AppTextStyles.caption1(context),
-                ),
-                SizedBox(height: 16),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    MediaButton(title: 'Google', image: Assets.imagesGoogle),
-                    MediaButton(
-                      title: 'Facebook',
-                      image: Assets.imagesFacebook,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 90),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: LogInViewBody(),
     );
   }
 }
---
+++ lib/features/auth/login/presentation/view/widgets/login_view_body.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../../../constant.dart';
+import '../../../../../../core/utils/app_text_styles.dart';
+import '../../../../../../core/widgets/bottom_navigation_bar/bottom_nvaigation_view.dart';
+import '../../../../../../core/widgets/custom_body_screen.dart';
+import '../../../../../../core/widgets/custom_button.dart';
+import '../../../../../../generated/assets.dart';
+import '../../../../../../generated/l10n.dart';
+import '../../../../sign_up/presentation/view/forgot_view.dart';
+import 'input_section_from_login_view.dart';
+import 'login_with_media_section.dart';
+
+class LogInViewBody extends StatelessWidget {
+  const LogInViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: CustomBodyScreen(
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 16,
+            children: [
+              SizedBox(height: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Text(
+                          S.of(context).welcomeBack,
+                          style: AppTextStyles.headline1(
+                            context,
+                          ).copyWith(color: Color(0xFF4F4F4F)),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      SvgPicture.asset(Assets.imagesEmojiSmiling),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 24),
+              InputSectionFromLogInView(),
+              Align(
+                alignment:
+                isArabic == 'ar'
+                    ? Alignment.centerLeft
+                    : Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, ForgetView.routeName);
+                  },
+                  child: Text(
+                    S.of(context).forgotPassword,
+                    style: AppTextStyles.caption1(context),
+                  ),
+                ),
+              ),
+              SizedBox(),
+              CustomButton(
+                title: S.of(context).signIn,
+                onPressed: () {
+                  Navigator.pushReplacementNamed(
+                    context,
+                    BottomNavigationView.routeName,
+                  );
+                },
+              ),
+              SizedBox(height: 12),
+
+              LogInWithMediaSection(),
+              SizedBox(height: 90),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
