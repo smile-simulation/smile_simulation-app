1e7248d77d40160484a94ece238e03a2d6f83964
mohamed <mezoattalla123@gmail.com>
2025-03-25 02:29:22 +0200

    🎨 [UI](ios/web/lib): Updates launch screen and splash screen assets

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -16,6 +16,7 @@ class Assets {
   static const String imagesFacebook = 'assets/images/facebook.svg';
   static const String imagesGoogle = 'assets/images/google.svg';
   static const String imagesInput = 'assets/images/input.svg';
+  static const String imagesLogo = 'splash.png';
   static const String imagesManageSignUpBackground = 'assets/images/manageSignUpBackground.png';
   static const String imagesManageSignup = 'assets/images/manageSignup.png';
   static const String imagesOnBoarding1 = 'assets/images/onBoarding1.png';
@@ -29,7 +30,6 @@ class Assets {
   static const String imagesOnBoardingShap4 = 'assets/images/onBoardingShap4.png';
   static const String imagesPostImage = 'assets/images/post_image.png';
   static const String imagesScanCamera = 'assets/images/scan-camera.svg';
-  static const String imagesSplash = 'assets/images/splash.png';
   static const String imagesSplashAndroid12Logo = 'assets/images/splash_android12_logo.png';
 
 }
