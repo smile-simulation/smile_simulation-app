b95e14b87a65d3cd6409530a4fad6622c83ce075
mohamed <mezoattalla123@gmail.com>
2025-04-01 18:54:41 +0200

    ✨ [FEAT](core/api): Refactor API consumer and error handling ♻️ [REFACTOR](core): Update error and exception handling ✨ [FEAT](core/errors): Implement error model and handling ✨ [FEAT](core/api): Add API keys and endpoints for authentication ♻️ [REFACTOR](core/api): Update API interceptor and endpoints ♻️ [REFACTOR](user_account): Update user details repository

--- lib/core/helper_functions/on_generate_route.dart
+++ lib/core/helper_functions/on_generate_route.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smile_simulation/core/api/dio_consumer.dart';
+import 'package:smile_simulation/core/helper_functions/get_it.dart';
 import 'package:smile_simulation/features/auth/login/presentation/view/login_view.dart';
 
 import 'package:smile_simulation/core/widgets/bottom_navigation_bar/bottom_nvaigation_view.dart';
+import 'package:smile_simulation/features/auth/sign_up/presentation/manage/cubits/sign_up_user_cubit.dart';
 
 import 'package:smile_simulation/features/auth/sign_up/presentation/view/manage_sign_up.dart';
 
@@ -23,7 +27,15 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (_) => const ManageSignUpView());
 
     case SignUpFromUserView.routeName:
-      return MaterialPageRoute(builder: (_) => const SignUpFromUserView());
+      return MaterialPageRoute(
+        builder:
+            (_) => BlocProvider(
+              create:
+                  (context) =>
+                      SignUpUserCubit(dioConsumer: getIt<DioConsumer>(),),
+              child: SignUpFromUserView(),
+            ),
+      );
 
     case SignUpFromDoctorView.routeName:
       return MaterialPageRoute(builder: (_) => const SignUpFromDoctorView());
