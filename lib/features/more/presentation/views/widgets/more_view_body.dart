e9f3b7f2fffb09c86cae6b6faac367500344ffc4
Mahmoud-Magdy17 <MahoudMagdy176203@gmail.com>
2025-04-04 10:46:08 +0200

    set internet permission

--- lib/features/more/presentation/views/widgets/more_view_body.dart
+++ lib/features/more/presentation/views/widgets/more_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:smile_simulation/core/api/api_keys.dart' show ApiKeys;
+import 'package:smile_simulation/core/database/cache/cache_helper.dart';
 import 'package:smile_simulation/core/utils/app_colors.dart';
 import 'package:smile_simulation/core/utils/app_text_styles.dart';
+import 'package:smile_simulation/features/auth/login/presentation/view/login_view.dart';
 
 class MoreViewBody extends StatelessWidget {
   const MoreViewBody({super.key});
@@ -10,7 +13,10 @@ class MoreViewBody extends StatelessWidget {
     return Center(
       child: TextButton(
         style: TextButton.styleFrom(backgroundColor: AppColors.whiteColor),
-        onPressed: () {},
+        onPressed: () {
+          CacheHelper().removeData(key: ApiKeys.token);
+          Navigator.pushReplacementNamed(context, LoginView.routeName);
+        },
         child: Text(
           "Losgout",
           style: AppTextStyles.button2(
