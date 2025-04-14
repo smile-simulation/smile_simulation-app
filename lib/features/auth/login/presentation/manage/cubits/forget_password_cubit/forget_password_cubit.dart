0ce2b1babfc4dc5f3e1d03e98363a925c351340a
mohamed <mezoattalla123@gmail.com>
2025-04-14 15:03:18 +0200

    ✨ feat(auth): Add forget password cubit and states

---
+++ lib/features/auth/login/presentation/manage/cubits/forget_password_cubit/forget_password_cubit.dart
@@ -0,0 +1,56 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../data/repos/forget_repos/forget_password_repo.dart';
+
+part 'forget_password_state.dart';
+
+class ForgetPasswordCubit extends Cubit<ForgetPasswordState> {
+  ForgetPasswordCubit(this.forgetPasswordRepo) : super(ForgetPasswordInitial());
+
+  final ForgetPasswordRepo forgetPasswordRepo;
+
+  void sendCode({required String email}) async {
+    emit(SendCodeLoading());
+
+    final result = await forgetPasswordRepo.forgetPassword(email: email);
+    result.fold(
+      (l) => emit(SendCodeFailure(l.errorMessage)),
+      (r) => emit(SendCodeSuccess(r.message!)),
+    );
+  }
+
+  confirmNum({required String email, required String codeOTP}) async {
+    emit(ConfirmNumLoading());
+
+    final result = await forgetPasswordRepo.verifyOTP(
+      email: email,
+      codeOTP: codeOTP,
+    );
+
+    result.fold(
+      (l) => emit(ConfirmNumFailure(l.errorMessage)),
+      (r) => emit(ConfirmNumSuccess(r.message!)),
+    );
+  }
+
+  Future<void> changePassword({
+    required String email,
+    required String token,
+    required String newPassword,
+    required String confirmPassword,
+  }) async {
+    emit(ChangePasswordLoading());
+    final result = await forgetPasswordRepo.resetPassword(
+      email: email,
+      token: token,
+      password: newPassword,
+      confirmPassword: confirmPassword,
+    );
+
+    result.fold(
+      (l) => emit(ChangePasswordFailure(l.errorMessage)),
+      (r) => emit(ChangePasswordSuccess(r.message!)),
+    );
+  }
+}
