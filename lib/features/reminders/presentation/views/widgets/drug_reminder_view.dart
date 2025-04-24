96d99b66051ce7dcabae35a7f67526cc22f1da19
MohamedHamed <mohamedhamedabdulhakam@gmail.com>
2025-04-24 21:30:15 +0200

    changes in reminder view body

---
+++ lib/features/reminders/presentation/views/widgets/drug_reminder_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrugReminderView extends StatelessWidget {
+  const DrugReminderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
