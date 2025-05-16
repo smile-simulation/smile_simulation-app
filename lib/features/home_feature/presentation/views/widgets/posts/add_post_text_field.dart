fe3268487345342367537b346fd14ffe5825fa79
mohamed <mezoattalla123@gmail.com>
2025-05-16 19:21:32 +0300

    ✨ feat(home_feature): Enhance create post functionality
    - Implemented clear image functionality in AddPostCubit.
    - Refactored AddPostTextField to be a stateless widget.
    - Improved CreatePostViewBody layout using CustomScrollView and SliverFillRemaining.
    - Added a close button to remove picked images.
    - Navigates to BottomNavigationView after successful post creation.
    - Updated CreatePostView app bar to use customAppbar widget.
    - Adjusted text styles and spacing for better UI.
    - Updated Arabic and English translations for "Create post".

--- lib/features/home_feature/presentation/views/widgets/posts/add_post_text_field.dart
+++ lib/features/home_feature/presentation/views/widgets/posts/add_post_text_field.dart
@@ -6,8 +6,11 @@ import 'package:smile_simulation/features/home_feature/presentation/cubits/add_p
 import 'package:smile_simulation/generated/l10n.dart';
 
 class AddPostTextField extends StatefulWidget {
-  const AddPostTextField({super.key});
+  const AddPostTextField({super.key, required this.controller, required this.onPressed});
 
+
+   final TextEditingController controller;
+    final Function() onPressed;
   @override
   State<AddPostTextField> createState() => _AddPostTextFieldState();
 }
@@ -15,40 +18,34 @@ class AddPostTextField extends StatefulWidget {
 class _AddPostTextFieldState extends State<AddPostTextField> {
   @override
   Widget build(BuildContext context) {
-    AddPostCubit cubit = context.read<AddPostCubit>();
-    return BlocBuilder<AddPostCubit, AddPostState>(
-      builder: (context, state) {
-        return TextField(
-          controller: cubit.contentController,
-          maxLines: null,
-          minLines: 1,
-          decoration: InputDecoration(
-            suffixIcon: IconButton(
-              onPressed: () async {
-                await cubit.pickImage();
-              },
-              icon: Icon(Icons.image, color: AppColors.primaryColor),
-            ),
-            hintText: S.of(context).postContent,
-            hintStyle: AppTextStyles.headline2(
-              context,
-            ).copyWith(color: AppColors.greyColor),
+    return TextField(
+      controller:  widget.controller,
+      maxLines: null,
+      minLines: 1,
+      decoration: InputDecoration(
+        suffixIcon: IconButton(
+          onPressed: widget.onPressed,
+          icon: Icon(Icons.image, color: AppColors.primaryColor),
+        ),
+        hintText: S.of(context).postContent,
+        hintStyle: AppTextStyles.headline2(
+          context,
+        ).copyWith(color: AppColors.greyColor),
 
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: AppColors.transparent),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: AppColors.primaryColor,
-                width: 2.0,
-              ), // Border color when focused
-            ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white.withValues()),
-            ),
-          ),
-        );
-      },
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.transparent),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.transparent,
+            width: 2.0,
+          ), // Border color when focused
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white.withValues()),
+        ),
+      ),
     );
+
   }
 }
