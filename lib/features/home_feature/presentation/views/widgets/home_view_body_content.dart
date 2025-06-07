73f4fc4331d4adc8cf994b5a9edbefe829afb512
Mahmoud-Magdy17 <MahoudMagdy176203@gmail.com>
2025-06-07 12:34:28 +0300

    solve scheduling issue

--- lib/features/home_feature/presentation/views/widgets/home_view_body_content.dart
+++ lib/features/home_feature/presentation/views/widgets/home_view_body_content.dart
@@ -3,12 +3,16 @@ import 'package:smile_simulation/core/services/local_notification_service.dart';
 import 'package:smile_simulation/core/utils/app_colors.dart';
 import 'package:smile_simulation/core/utils/app_text_styles.dart';
 import 'package:smile_simulation/generated/l10n.dart';
-import 'package:timezone/timezone.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 import 'posts_list_view.dart';
 
 class HomeViewBodyContent extends StatelessWidget {
   const HomeViewBodyContent({super.key});
+
+  tz.TZDateTime convertToTZDateTime(DateTime dateTime) {
+    return tz.TZDateTime.from(dateTime, tz.local);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,11 +54,12 @@ class HomeViewBodyContent extends StatelessWidget {
                       pickedTime.minute,
                     );
 
-                    LocalNotificationService.scheduleNotification(
-                      dateTime: dateTime,
+                    LocalNotificationService.scheduleReminderNotification(
                       title: "هذا اشعار مجدول",
                       body: 'هذا الاشعار مجدول لوقت معين ',
                       payload: '',
+                      id: 5,
+                      scheduledDate: convertToTZDateTime(dateTime),
                     );
                   }
                 },
