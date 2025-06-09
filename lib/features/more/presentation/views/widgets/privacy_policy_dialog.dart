72118a419291b6b5d62294082eeb1308b6909217
Mahmoud-Magdy17 <MahoudMagdy176203@gmail.com>
2025-06-09 11:30:42 +0300

    localization of terms and conditions

--- lib/features/more/presentation/views/widgets/privacy_policy_dialog.dart
+++ lib/features/more/presentation/views/widgets/privacy_policy_dialog.dart
@@ -24,81 +24,78 @@ class CustomPoliciesAndConditionsDialog extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(radius),
       ),
-      child: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.6,
-
-
-          decoration: BoxDecoration(
-            color: AppColors.whiteColor,
-            borderRadius: BorderRadius.circular(radius),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                title,
-                style: AppTextStyles.headline2(context).copyWith(
-                  decoration: TextDecoration.underline,
-                  color: AppColors.primaryColor,
-                ),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.6,
+      
+      
+        decoration: BoxDecoration(
+          color: AppColors.whiteColor,
+          borderRadius: BorderRadius.circular(radius),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: AppTextStyles.headline2(context).copyWith(
+                decoration: TextDecoration.underline,
+                color: AppColors.primaryColor,
               ),
-              SizedBox(height: 8),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric( horizontal:  8.0),
-                  child: FutureBuilder<String>(
-                    future: DefaultAssetBundle.of(context).loadString(path),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        if (snapshot.hasError || !snapshot.hasData) {
-                          log('Error loading file: ${snapshot.error}');
-                          return const Center(
-                            child: Text('Error loading policy'),
-                          );
-                        }
-
-                        return MarkdownWidget(
-                          padding: EdgeInsets.all(8),
-                          data: snapshot.data!,
-                          shrinkWrap: true,
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric( horizontal:  8.0),
+                child: FutureBuilder<String>(
+                  future: DefaultAssetBundle.of(context).loadString(path),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasError || !snapshot.hasData) {
+                        log('Error loading file: ${snapshot.error}');
+                        return const Center(
+                          child: Text('Error loading policy'),
                         );
                       }
-
-                      return const Center(child: CircularProgressIndicator());
-                    },
+      
+                      return MarkdownWidget(
+                        padding: EdgeInsets.all(8),
+                        data: snapshot.data!,
+                        shrinkWrap: true,
+                      );
+                    }
+      
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                backgroundColor: AppColors.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(radius),
+                    bottomRight: Radius.circular(radius),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                  backgroundColor: AppColors.primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(radius),
-                      bottomRight: Radius.circular(radius),
-                    ),
-                  ),
-                ),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: double.infinity,
-                  child: Text(
-                    S.of(context).close,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.whiteColor,
-                    ),
+              child: Container(
+                alignment: Alignment.center,
+                height: 50,
+                width: double.infinity,
+                child: Text(
+                  S.of(context).close,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.whiteColor,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
